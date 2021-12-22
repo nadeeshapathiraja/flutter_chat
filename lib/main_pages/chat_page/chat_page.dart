f1ce5602b243cd6fcdf6051ac6c7e15b7e468166
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-12-22 18:01:02 +0530

    done chat UI view and add parts

--- lib/main_pages/chat_page/chat_page.dart
+++ lib/main_pages/chat_page/chat_page.dart
@@ -1,4 +1,3 @@
-import 'package:chat_app_flutter/components/custom_image.dart';
 import 'package:chat_app_flutter/components/custom_text.dart';
 import 'package:chat_app_flutter/utils/app_colors.dart';
 import 'package:chat_app_flutter/utils/constants.dart';
@@ -31,75 +30,99 @@ class ChatTileSetion extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+    return GestureDetector(
+      onTap: () {},
       child: Container(
-        width: double.infinity,
-        height: 70,
-        child: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(1000.0),
-                child: Image.asset(
-                  Constants.imageAssets("person.jpg"),
-                  width: 60,
-                  height: 60,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CustomText(
-                        text: "Sumudu Sadakelum",
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      SizedBox(height: 5),
-                      CustomText(
-                        text: "Have a good one!",
-                        fontSize: 14,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 1),
+        child: Container(
+          width: double.infinity,
+          height: 70,
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              children: [
+                Stack(
                   children: [
-                    Column(
-                      children: [
-                        CustomText(
-                          text: "2021-12-31",
-                          fontSize: 13,
-                        ),
-                        CustomText(
-                          text: "4.30 pm",
-                          fontSize: 12,
-                        )
-                      ],
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(1000.0),
+                      child: Image.asset(
+                        Constants.imageAssets("person.jpg"),
+                        width: 60,
+                        height: 60,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                    Icon(
-                      Icons.done_all,
-                      color: kblue,
-                      size: 14,
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 6, vertical: 2),
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: kDarkBluelack),
+                        alignment: Alignment.center,
+                        child: CustomText(
+                          text: "2",
+                          color: kwhite,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     )
                   ],
                 ),
-              ),
-            ],
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CustomText(
+                          text: "Sumudu Sadakelum",
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        SizedBox(height: 5),
+                        CustomText(
+                          text: "Have a good one!",
+                          fontSize: 14,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          CustomText(
+                            text: "2021-12-31",
+                            fontSize: 13,
+                          ),
+                          CustomText(
+                            text: "4.30 pm",
+                            fontSize: 12,
+                          )
+                        ],
+                      ),
+                      Icon(
+                        Icons.done_all,
+                        color: kblue,
+                        size: 14,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        decoration: BoxDecoration(
-          border: Border.all(),
-          borderRadius: BorderRadius.circular(15),
+          decoration: BoxDecoration(
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(15),
+          ),
         ),
       ),
     );
--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 const kwhite = Colors.white;
 const kblue = Colors.blue;
 const kBlack = Colors.black;
+const kRed = Colors.red;
+const kDarkBluelack = Color(0xff0000b3);
 const kgreen = Color(0xff6DC54A);
