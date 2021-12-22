68d6121ffae1fb164e42d496610e546df0a32960
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-12-22 22:16:34 +0530

    done Call Page UI view and add parts

--- lib/main_pages/call_page/call_page.dart
+++ lib/main_pages/call_page/call_page.dart
@@ -1,3 +1,6 @@
+import 'package:chat_app_flutter/components/custom_text.dart';
+import 'package:chat_app_flutter/utils/app_colors.dart';
+import 'package:chat_app_flutter/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class CallPage extends StatefulWidget {
@@ -10,8 +13,93 @@ class CallPage extends StatefulWidget {
 class _CallPageState extends State<CallPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Call Page"),
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 1),
+        child: Container(
+          width: double.infinity,
+          height: 70,
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(1000.0),
+                  child: Image.asset(
+                    Constants.imageAssets("person.jpg"),
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CustomText(
+                          text: "Sumudu Sadakelum",
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          CustomText(
+                            text: "2021-12-31",
+                            fontSize: 13,
+                          ),
+                          CustomText(
+                            text: "4.30 pm",
+                            fontSize: 12,
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: () {},
+                            child: const Icon(
+                              Icons.call,
+                              color: kblue,
+                              size: 20,
+                            ),
+                          ),
+                          const SizedBox(width: 50),
+                          InkWell(
+                            onTap: () {},
+                            child: const Icon(
+                              Icons.videocam,
+                              color: kblue,
+                              size: 25,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          decoration: BoxDecoration(
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+      ),
     );
   }
 }
