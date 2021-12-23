3f1e16642c6cb47e0ee9defc440fb7695a14080c
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-12-23 23:48:26 +0530

    done Call Page done

--- lib/main_pages/profile_page/profile_page.dart
+++ lib/main_pages/profile_page/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app_flutter/utils/app_colors.dart';
 import 'package:chat_app_flutter/utils/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -14,13 +15,24 @@ class _ProfilePageState extends State<ProfilePage> {
     final size = MediaQuery.of(context).size;
     return Stack(
       children: [
-        Container(
-          width: size.width * 0.5,
-          height: size.height * 0.2,
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(
-                "https://image.freepik.com/free-photo/indoor-shot-beautiful-happy-african-american-woman-smiling-cheerfully-keeping-her-arms-folded-relaxing-indoors-after-morning-lectures-university_273609-1270.jpg"),
-            radius: 60.0,
+        Center(
+          child: Container(
+            width: size.width * 0.9,
+            height: size.height / 2,
+            color: kgreen,
+          ),
+        ),
+        Positioned(
+          top: 40,
+          left: size.width * 0.2,
+          child: Container(
+            width: size.width * 0.6,
+            height: size.height * 0.2,
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://image.freepik.com/free-photo/indoor-shot-beautiful-happy-african-american-woman-smiling-cheerfully-keeping-her-arms-folded-relaxing-indoors-after-morning-lectures-university_273609-1270.jpg"),
+              radius: 60.0,
+            ),
           ),
         ),
       ],
